567430dbcbb3342652e3239048c3ac6c2034bd4b
Meghan Grousset <megh.g@hotmail.fr>
2023-03-29 12:05:12 +0200

    avec html horaires

---
+++ horaires.js
@@ -0,0 +1,107 @@
+const params = (new URL(document.location)).searchParams
+
+async function loadPage(){
+    // On récupère les horaires à l'arrêt 
+    let url = "https://open.tan.fr/ewp/horairesarret.json/" + params.get("arret") + "/" + params.get("ligne") + "/" + params.get("sens")
+    let result = await fetchAPI(url)
+    console.log(result)
+    
+    // On affiche l'histogramme
+    createHistogramFrom(result)
+    // On affiche les horaires sous fome textuelle
+    createHorairesTxtFrom(result)
+}
+
+// Affiche les horaires contenues dans result sous forme textuelle dans le html
+function createHorairesTxtFrom(result) {
+    
+    // 1ere solution
+    // Pour chaque horaire
+    result["horaires"].forEach(horaire => {
+        // On créer un paragraph
+        let node = document.createElement("p")
+
+        // On met l'heure en gras (% 24 car les heures >= 24 sont les heures du lendemain - 0h, 1h, ...)
+        let heure = parseInt(horaire["heure"].replace(/\D/, "")) % 24 + "h"
+        let minutes = horaire["passages"].join(" | ")
+        node.innerHTML = "<b>" + heure + "</b> : " + minutes
+
+        // On ajoute le paragraph dans l'élement horairesText pour l'afficher
+        document.getElementById("horairesText").appendChild(node)
+    })
+
+    // 2ème solution
+    /*
+    result["horaires"]
+        .flatMap(x => getPassagesFrom(x))
+        .forEach(passage => {
+            document.getElementById("horairesText").innerHTML += passage.heure % 24 + "h" + passage.minute + "<br/>"
+        })
+    */
+}
+
+// Affiche les horaires contenues dans le result dans l'histogramme de la page
+function createHistogramFrom(result) {
+    // Donne une liste exploitable de tous les passages de l'arret
+    let allPassages = result["horaires"].flatMap(horaire => getPassagesFrom(horaire))
+    
+    // Equivalent au code suivant: 
+    /*
+    let allPassages = []
+    for (let horaire of result["horaires"]) {
+        for (let passage of horaire["passages"]) {
+            allPassages.push({
+                heure: parseInt(horaire["heure"].replace(/\D/, "")), 
+                minute: parseInt(passage.replace(/\D/, ""))
+            })
+        }
+    }
+    console.log(passages)
+    */
+
+    // Affichage de l'histogramme
+    let dataHistogram = allPassages.map(x => x.heure)
+    // Les données à afficher dans l'histogramme
+    let data = {
+        x: dataHistogram,
+        xbins: {
+            size: 1
+        },
+        type: 'histogram',
+    }
+
+    // On récupère le terminus
+    let terminus = result["ligne"]["directionSens1"]
+    if (params.get("sens") != 1) {
+        terminus = result["ligne"]["directionSens2"]
+    }
+    
+    // On ajoute des élement de décoration, ici un titre
+    let layout = {
+        title: "Arrêt " + result["arret"]["libelle"] + " : ligne " + result["ligne"]["numLigne"] + " vers " + terminus
+    }
+
+    // On affiche l'histogramme
+    Plotly.newPlot('myDiv', [data], layout);
+}
+
+// Donne une liste de passages pour un horaire donné
+// La liste donne des élements sous la forme { heure: x, minute: y }
+function getPassagesFrom(horaire) {
+    return horaire["passages"]
+        .map(passage => { 
+            return {
+                heure: parseInt(horaire["heure"].replace(/\D/, "")), // Garde juste l'heure sous forme d'entier (ex: "13h" => 13)
+                minute: parseInt(passage.replace(/\D/, "")) // Garde juste les minutes sous forme d'entier (ex: "59s" => 59)
+            }
+        })
+}
+
+async function fetchAPI(url) {
+    let response = await fetch(url)
+    return await response.json() 
+}
+
+loadPage()
+
+
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// fuction horloge, heure
+// function horloge, heure
 
 let html = document.getElementById("time");
 
@@ -22,82 +22,3 @@ setInterval(function () {
   html.innerHTML = heu + " : " + min + " : " + seg;
 }, 1000);
 
-
- // function map
-//  cree variable map . et appelle la librerie L.map que va importe le map 
-// avec les cordone de la ville , cartier , au pays 
-
- let map = L.map("map")
- map.setView([47.2173, -1.5534], 15);
-  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
- //let marker = L.marker([47.2173, -1.5534]).addTo(map);
-
-// function de geolocalitation 
-/* navigator.geolocation.watchPosition(success,error);
-
-function success(pos) {
-
-    const lati = pos.coords.latitude;
-    const long = pos.coords.longitude;
-    const accuracy = pos.coords.accuracy;
-    //let stations = await getStations(locationResult.coords)
-
-   let marker = L.marker([lati,long]).addTo(map)
-   let circlet= L.circle([lati,long],{ radius:accuracy}).addTo(map);
-
-    map.fitBounds(circlet.getBounds());
-}
-
-function error (err) {
-    if (err.code ===1){
-        alert ( "accepte geolocalitation")
-    }else{
-        alert ("votre dispositive ne permet pas la geolocalition ")
-    }
-    
-}
- */
-
-
-//fonction appelée si l'utilisateur autorise la géolocalisation du navigateur et reçoit en paramètres
-// la localisation
-
-      navigator.geolocation.watchPosition(success,error);
-
-     async function success(locationResult) {
-         const lati = locationResult.coords.latitude;
-         const long =locationResult.coords.longitude;
-         const accuracy = locationResult.coords.accuracy;
-             console.log("User is at " + lati + ":" + long + ":" + accuracy )
-         let marker = L.marker([lati,long,accuracy]).addTo(map)
-         let circlet= L.circle([lati,long,accuracy],{radius:accuracy}).addTo(map);
-         //let stations = await getStations(locationResult.coords).addTo(map)
-     map.fitBounds(circlet.getBounds());
-             console.log(stations)
-     }
-
-     function error (err) {
-         if (err.code ===1){
-             alert ( "accepte geolocalitation")
-         }else{
-             alert ("votre dispositive ne permet pas la geolocalition ")
-         }
-        
-     }
-    
-
-     async function getStations(coords) {
-        let url = "https://open.tan.fr/ewp/arrets.json/" + coords.latitude + "/" + coords.longitude .addTo(map)
-    
-        console.log(url)    
-        //contient la réponse de la requête (ici localisation)
-        const response = await fetch (url)
-        //contient la réponse en format .json 
-        const arrets = await response.json()
-        
-        return arrets 
-    } 
